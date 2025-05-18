35cb13b39c783d7ea77c01a9985a7bc87b425b6a
Lucas Felipe <lucasfelipetimoteo@gmail.com>
2025-05-18 12:19:48 -0300

    Fix interestPerMonth. From 2 to 3

--- src/infra/repository/typeORM/entity/Simulation.ts
+++ src/infra/repository/typeORM/entity/Simulation.ts
@@ -1,15 +1,13 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity()
+export const simulationsTableName =
+	process.env.NODE_ENV === "test" ? "simulations_test" : "simulations";
+
+@Entity({ name: simulationsTableName })
 export class Simulation {
 	@PrimaryGeneratedColumn("uuid")
 	id!: string;
 
-	// TODO: A TIPAGEM GRADA PELA RELAÇÃO GERA CONFLITO COM A TIPAGEM ESPERADA, QUEBRANDO A INDEPENDÊNCIA DE LIBS EXTERNAS
-	// - manter sem relacionamento. Se der tempo, resolver esse problema
-
-	// @ManyToOne(() => Student, { nullable: false })
-	// @JoinColumn({ name: "studentId", referencedColumnName: "id" })
 	@Column({ nullable: false })
 	studentId!: string;
 
@@ -19,7 +17,7 @@ export class Simulation {
 	@Column({ nullable: false })
 	installmentsQuantity!: number;
 
-	@Column("decimal", { precision: 5, scale: 2, nullable: false })
+	@Column("decimal", { precision: 5, scale: 3, nullable: false })
 	interestPerMonth!: number;
 
 	@Column("decimal", { precision: 15, scale: 2, nullable: false })
